f5dc95ec08fb2cb49b885aafa3c12ccc5aa00987
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-12 17:39:42 +0900

    feat: router

---
+++ backend/internal/interface/http/router.go
@@ -0,0 +1,39 @@
+package http
+
+import (
+	"example.com/m/internal/infrastructure/env"
+	"example.com/m/internal/interface/http/compute"
+	"example.com/m/internal/interface/http/user"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func InitRoutes(
+	userHandler *user.Handler,
+	computeHandler *compute.Handler,
+) *echo.Echo {
+	e := echo.New()
+
+	// セッションミドルウェアのDefault設定
+	secret := env.GetString("SESSION_SECRET", "secret")
+	store := sessions.NewCookieStore([]byte(secret))
+	store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+		Secure:   false,
+	}
+	e.Use(session.Middleware(store))
+
+
+	// ユーザ関連のルートを登録
+	userGroup := e.Group("/user")
+	userHandler.RegisterRoutes(userGroup)
+
+	// コンピュート関連のルートを登録
+	computeGroup := e.Group("/compute")
+	computeHandler.RegisterRoutes(computeGroup)
+
+	return e
+}
