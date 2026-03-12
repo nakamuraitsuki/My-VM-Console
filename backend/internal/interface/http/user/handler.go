1b526479194638ee26df94c93fede9a86e0f6cfb
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-12 17:14:11 +0900

    feat: loginだけ

---
+++ backend/internal/interface/http/user/handler.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"example.com/m/internal/infrastructure/oidc"
+	"example.com/m/internal/usecase/user"
+)
+
+type Handler struct {
+	oidcConfig        *oidc.OIDCConfig
+	idTokenVerifier   oidc.IDTokenVerifier
+	ensureUserUseCase user.EnsureUserUseCase
+}
+
+func NewHandler(
+	oidcConfig *oidc.OIDCConfig,
+	idTokenVerifier oidc.IDTokenVerifier,
+	ensureUserUseCase user.EnsureUserUseCase,
+) *Handler {
+	return &Handler{
+		oidcConfig:        oidcConfig,
+		idTokenVerifier:   idTokenVerifier,
+		ensureUserUseCase: ensureUserUseCase,
+	}
+}
