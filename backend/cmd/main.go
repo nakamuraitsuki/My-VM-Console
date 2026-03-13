d5a10ed55f1287ab9efc0ce77f5fc1205baa019d
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-13 18:19:06 +0900

    feat: main完成

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	networkDomain "example.com/m/internal/domain/network"
+	authHttp "example.com/m/internal/infrastructure/auth/http"
+	"example.com/m/internal/infrastructure/auth/mapper"
 	"example.com/m/internal/infrastructure/auth/oidc"
+	"example.com/m/internal/infrastructure/driver/etcd"
+	gatewayDriverInfra "example.com/m/internal/infrastructure/driver/etcd/gateway"
+	"example.com/m/internal/infrastructure/driver/incus"
+	computeDriverInfra "example.com/m/internal/infrastructure/driver/incus/compute"
+	networkDriverInfra "example.com/m/internal/infrastructure/driver/incus/network"
+	storageDriverInfra "example.com/m/internal/infrastructure/driver/incus/storage"
+	"example.com/m/internal/infrastructure/env"
+	"example.com/m/internal/infrastructure/messaging/channel"
 	"example.com/m/internal/infrastructure/persistence/sqlite"
 	computeRepoInfra "example.com/m/internal/infrastructure/persistence/sqlite/compute"
 	gatewayRepoInfra "example.com/m/internal/infrastructure/persistence/sqlite/gateway"
@@ -10,9 +24,10 @@ import (
 	networkRepoInfra "example.com/m/internal/infrastructure/persistence/sqlite/network"
 	storageRepoInfra "example.com/m/internal/infrastructure/persistence/sqlite/storage"
 	userRepoInfra "example.com/m/internal/infrastructure/persistence/sqlite/user"
-	"example.com/m/internal/interface/http"
+	httpAdapter "example.com/m/internal/interface/http"
 	computeH "example.com/m/internal/interface/http/compute"
 	userH "example.com/m/internal/interface/http/user"
+	"example.com/m/internal/usecase"
 	computeUC "example.com/m/internal/usecase/compute"
 	networkUC "example.com/m/internal/usecase/network"
 	userUC "example.com/m/internal/usecase/user"
@@ -29,7 +44,7 @@ func main() {
 	oidcVerifier := oidc.NewIDTokenVerifier(oidcCfg)
 
 	// repository
-	// uow := sqlite.NewUnitOfWork(db)
+	uow := sqlite.NewTransactor(db)
 	userRepo := userRepoInfra.NewRepository(db)
 	instRepo := computeRepoInfra.NewRepository(db)
 	netRepo := networkRepoInfra.NewRepository(db)
@@ -38,21 +53,46 @@ func main() {
 	ingressRepo := gatewayRepoInfra.NewRepository(db)
 
 	// service
+	// TODO: 権限のマッピングのためのJSONを用意すること
+	filePath := env.GetString("EXTERNAL_AUTH_PERMISSION_MAPPING_FILE", "config/permission_mapping.yaml")
+	apiEndpoint := env.GetString("OAUTH_API_ENDPOINT", "")
+	permissionMapper := mapper.NewPermissionMapper(filePath, 3*time.Hour)
 	netCalcuSvc := networkDomain.NewNetworkService()
-	// identitySvc := userSvcInfra.NewService()
+	identitySvc := authHttp.NewExternalIdentityService(http.DefaultClient, apiEndpoint, permissionMapper)
+
+	// driver
+	incusConfig := incus.NewConfig()
+	incusClient := incus.NewClient(incusConfig)
+	instDriver := computeDriverInfra.NewDriver(incusClient)
+	netDriver := networkDriverInfra.NewDriver(incusClient)
+	volDriver := storageDriverInfra.NewDriver(incusClient)
+
+	etcdConfig := etcd.NewConfig()
+	etcdClient := etcd.NewClient(etcdConfig)
+	ingressDriver := gatewayDriverInfra.NewDriver(etcdClient)
+
+	// worker
+	hub := channel.NewHub()
+	publisher := channel.NewPublisher(hub)
+	subscriber := channel.NewSubscriber(hub)
 
 	// usecase
-	// ensureUserUC := userUC.NewEnsureUserInteractor(userRepo, netRepo, /* identitySvc */ netCalcuSvc, /* publisher */, /* uow */)
+	ensureUserUC := userUC.NewEnsureUserInteractor(userRepo, netRepo, identitySvc, netCalcuSvc, publisher, uow)
 	listMyInstanceUC := userUC.NewListMyInstanceInteractor(instRepo)
-	// provisioningNetUC := networkUC.NewProvisioningNetworkInteractor(userRepo, netRepo, netCalcuSvc, /* identitySvc */, /* dvier */, /* uow */)
-	// reqCreateUC := computeUC.NewRequestCreateInstanceInteractor(userRepo, instRepo, netRepo, ingressRepo, volRepo, netCalcuSvc, /* publisher */, /* uow */)
-	// execCreateUC := computeUC.NewExecuteCreateInstanceInteractor(instRepo, volRepo, ingressRepo, /* instDriver */, /* volDriver */, /*ingressDriver */, /* uow */)
-	
-	// handler
-	// userHandler := userH.NewHandler(oidcCfg, *oidcVerifier, /* ensureUserUC */, listMyInstanceUC)
-	// computeHandler := computeH.NewHandler(/* reqCreateUC */, /* ensureUserUC */)
+	provisioningNetUC := networkUC.NewProvisioningNetworkInteractor(userRepo, netRepo, netCalcuSvc, identitySvc, netDriver, uow)
+	reqCreateUC := computeUC.NewRequestCreateInstanceInteractor(userRepo, instRepo, netRepo, ingressRepo, volRepo, netCalcuSvc, publisher, uow)
+	execCreateUC := computeUC.NewExecuteCreateInstanceInteractor(instRepo ,netRepo, volRepo, ingressRepo, imgRepo, instDriver, volDriver, ingressDriver, uow)
 
+	// handler
+	userHandler := userH.NewHandler(oidcCfg, *oidcVerifier, ensureUserUC, listMyInstanceUC)
+	computeHandler := computeH.NewHandler(reqCreateUC, ensureUserUC)
+
+	// bind job handlers
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	usecase.Bind(ctx, subscriber, usecase.JobTypeCreateVPCAndDefaultSubnet, provisioningNetUC.Execute)
+	usecase.Bind(ctx, subscriber, usecase.JobTypeCreateInstance, execCreateUC.Execute)
 	// router
-	// e := http.InitRoutes(/* userHandler */, /* computeHandler */)
-	// e.Start(":8080")
+	e := httpAdapter.InitRoutes(userHandler, computeHandler)
+	e.Start(":8080")
 }
