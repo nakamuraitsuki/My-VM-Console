9f8425db55c99dd2b0c9a3ba2d98a6de91c874b9
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-15 17:16:24 +0900

    feat: domain　user　の再定義

--- frontend/src/domain/user/user.repository.ts
+++ frontend/src/domain/user/user.repository.ts
@@ -1,12 +1,6 @@
 import type { Result } from "../core/result";
+import type { User } from "./user.model";
 
 export interface IUserRepository {
-  updateProfile: (name: string | null, bio: string | null) => Promise<void>;
-  updateIcon: (icon: File | null) => Promise<Result<void, UpdateIconError>>;
+  getCurrentUser(): Promise<Result<User>>;
 }
-
-export type UpdateIconError =
-  | "INVALID_FORMAT"
-  | "FILE_TOO_LARGE"
-  | "NETWORK_ERROR"
-  | "UNKNOWN_ERROR";
